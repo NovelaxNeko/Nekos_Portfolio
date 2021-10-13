0e353278d6aca1b2bbe526da9c0b2e18f3d27daf
NovelaxNeko <42329190+NovelaxNeko@users.noreply.github.com>
2021-10-13 15:24:54 +0200

    Added automatic age stuff thingy...
    erh coding .-.

---
+++ js/age.js
@@ -0,0 +1,22 @@
+// The date you were born
+var birthDate = new Date(2030, 1, 29, 0, 0, 0, 0);
+
+// The current date
+var currentDate = new Date();
+
+// The age in years
+var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+// Compare the months
+var month = currentDate.getMonth() - birthDate.getMonth();
+
+// Compare the days
+var day = currentDate.getDate() - birthDate.getDate();
+
+// If the date has already happened this year
+if ( month < 0 || month == 0 && day < 0 )
+{
+    age--;
+}
+
+document.getElementById("age").innerHTML = age;
